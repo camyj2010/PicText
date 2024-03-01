459b1ebe8368ee0608ba8674bd4619c739ff0ef8
Sebastian <roncancio.juan@correounivalle.edu.co>
2024-03-01 18:09:08 -0500

    image square and visualization implemented

--- pictext_proyect/src/app/app.component.ts
+++ pictext_proyect/src/app/app.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, NgModule  } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,MatButtonModule],
+  imports: [RouterOutlet,MatButtonModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  imageUrl: string | undefined;
   uploadImage() {
     const input = document.createElement('input');
     input.type = 'file';
@@ -19,8 +21,20 @@ export class AppComponent {
       if (file) {
         console.log('File selected:', file);
         // Aquí se agrega la logica para cargar el archivo
+
+        const reader = new FileReader();
+        reader.onload = (e: any) => {
+          this.imageUrl = e.target.result;
+        };
+        reader.readAsDataURL(file);
       }
     });
     input.click();
   }
 }
+
+@NgModule({
+  imports: [CommonModule], // Agrega CommonModule al array de imports
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
