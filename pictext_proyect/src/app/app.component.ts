f5a015bce96b46b6d7dc884614f1e21e58a0cd1e
Cesar-velasquez117 <106538584+Cesar-velasquez117@users.noreply.github.com>
2024-03-01 17:14:52 -0500

    Implementation for the Upload button

--- pictext_proyect/src/app/app.component.ts
+++ pictext_proyect/src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'pictext_proyect';
+  uploadImage() {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.accept = 'image/*';
+    input.addEventListener('change', (event: any) => {
+      const file = event.target.files[0];
+      if (file) {
+        console.log('File selected:', file);
+        // Aquí se agrega la logica para cargar el archivo
+      }
+    });
+    input.click();
+  }
 }
