f072248594d8909c2a3863c871a598adb003ceb6
Andres Velasco <andresfelipecoronado@gmail.com>
2024-03-09 17:34:09 -0500

    Created the landing page and the register page, missing redirect to homepage in the landing page

--- pictext_proyect/src/app/app.routes.ts
+++ pictext_proyect/src/app/app.routes.ts
@@ -1,8 +1,20 @@
 import { Routes } from '@angular/router';
 import { PicTextComponent } from './pic-text/pic-text.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
 	{
 		path: 'pic-text', component: PicTextComponent
+	},
+	{
+		path: 'home', component: HomeComponent
+	},
+	{
+		path: 'register', component: RegisterComponent
+	},
+	{
+		path: 'login', component: LoginComponent
 	}
 ];
