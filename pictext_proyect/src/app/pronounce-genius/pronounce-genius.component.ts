c86e77a7584ba46d33b1a264fea6492179016d38
Sebastian <roncancio.juan@correounivalle.edu.co>
2024-03-28 13:15:15 -0500

    petition front to back implemented

--- pictext_proyect/src/app/pronounce-genius/pronounce-genius.component.ts
+++ pictext_proyect/src/app/pronounce-genius/pronounce-genius.component.ts
@@ -1,24 +1,39 @@
-import { Component, AfterViewInit, inject, PLATFORM_ID, Inject, OnInit} from '@angular/core';
-import {PDFDocument, rgb} from 'pdf-lib'
-import { FormsModule } from '@angular/forms'; // Importa FormsModule
-import { CommonModule } from '@angular/common'; // Importa CommonModule
-import {MatTableModule,MatTableDataSource} from '@angular/material/table';
-import { UserService } from '../user.service';
-import { SendCloudinaryService } from '../services/send-cloudinary.service';
-import { SendPictTextService } from '../services/send-pict-text.service';
-import { RouterLink } from '@angular/router';
-import { isPlatformBrowser } from '@angular/common';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-pronounce-genius',
   templateUrl: './pronounce-genius.component.html',
   styleUrls: ['./pronounce-genius.component.css']
 })
-export class PronounceGeniusComponent implements OnInit {
+export class PronounceGeniusComponent {
+  selectedFile: File | null = null;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {
+  onFileSelected(event: Event): void { 
+    console.log("on file selected")
+    const inputElement = event.target as HTMLInputElement;
+    if (inputElement.files && inputElement.files.length > 0) {
+      this.selectedFile = inputElement.files[0] as File;
+    }
   }
 
+  onSubmit(event: Event): void {
+    console.log("entre al onSubmit1");
+    event.preventDefault(); 
+
+    if (this.selectedFile) {
+      console.log("entre al onSubmit");
+      const formData = new FormData();
+      formData.append('audio', this.selectedFile);
+
+      this.http.post('http://127.0.0.1:8000/api/audio/', formData)
+        .subscribe(response => {
+          console.log('Archivo enviado correctamente', response); 
+        }, error => {
+          console.error('Error al enviar el archivo', error); 
+        });
+    }
+  }
 }
