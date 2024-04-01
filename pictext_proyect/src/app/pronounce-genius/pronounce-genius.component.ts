a65ea533b80c1f38d10946e6dbe3af8be89342d9
Cesar-velasquez117 <velasquez.cesar@correounivalle.edu.co>
2024-03-31 23:25:56 -0500

    Restoring getUserData function

--- pictext_proyect/src/app/pronounce-genius/pronounce-genius.component.ts
+++ pictext_proyect/src/app/pronounce-genius/pronounce-genius.component.ts
@@ -1,9 +1,11 @@
-import { ChangeDetectorRef, Component, ElementRef, Inject, OnInit, ViewChild, PLATFORM_ID } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, inject, Inject, OnInit, ViewChild, PLATFORM_ID, AfterViewInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RouterLink } from '@angular/router';
 import { isPlatformBrowser } from '@angular/common';
 import { AudioRecorderService } from '../services/audio-recorder.service';
 import { Subscription } from 'rxjs';
+import { UserService } from '../user.service';
+import { Record } from '../services/record-word.service';
 
 
 export interface WordResponse {
@@ -19,9 +21,17 @@ export interface WordResponse {
 	styleUrls: ['./pronounce-genius.component.css']
 })
 export class PronounceGeniusComponent implements OnInit{
-  selectedFile: File | null = null;
+	userService = inject(UserService)
+
+	ngAfterViewInit() {
+	  this.getUserData();
+	}
+	
+	selectedFile: File | null = null;
   displayedText: string = 'Welcome, please select the dificulty';
-	dificulty: string = '';
+  correctWords: string[] = [];
+  incorrectWords: string[] = [];	
+  dificulty: string = '';
 	userEmail: string = '';
 	private backendURL = 'http://127.0.0.1:8000/api'
 
@@ -34,10 +44,11 @@ export class PronounceGeniusComponent implements OnInit{
 		private http: HttpClient,
 		private audioRecordingService: AudioRecorderService, 
 		private cd: ChangeDetectorRef,
-		@Inject(PLATFORM_ID) private platformId: Object) { }
+		@Inject(PLATFORM_ID) private platformId: Object,
+		private record: Record) { }
 
   ngOnInit() {
-		this.getData()
+		this.getUserData()
     this.audioRecordingService.audioBlob$.subscribe(blob => {
 			this.audioBlobSubscription = blob
       this.audioURL = window.URL.createObjectURL(blob);
@@ -104,14 +115,44 @@ export class PronounceGeniusComponent implements OnInit{
 		}
 
 
-	getData(){
-    if (isPlatformBrowser(this.platformId)) {
-			const userEmail = sessionStorage.getItem('email');
-    	this.userEmail = userEmail !== null ? userEmail : '';
-		}else{
-			this.userEmail = '';
-		}
-  }
+		getUserData() {
+			// Get the Email of the user from sessionStorage
+			if(isPlatformBrowser(this.platformId)){
+			  const userEmail = sessionStorage.getItem('email');
+			  if(userEmail){
+				this.http.get<any>('http://127.0.0.1:8000/api/obtener/')
+				  .subscribe((response) => {
+					console.log('User data:', response);
+		
+					 // Find the user by email
+					 const userData = response.datos_usuarios.find((user: any) => user.email === userEmail);
+					 if(userData){
+					   console.log('Found user:', userData);
+		
+					   // Process userData to get correct and incorrect words
+					   const processedData = this.record.fetchWords(userData);
+					   this.correctWords = processedData.correctWords;
+					   this.incorrectWords = processedData.incorrectWords;
+		
+					   console.log('Correct words:', this.correctWords);
+					   console.log('Incorrect words:', this.incorrectWords);
+					   // Updates the divs information with the correct and incorrect words
+					   const correctTextBox = document.querySelector('.correct-text-box');
+					   const incorrectTextBox = document.querySelector('.incorrect-text-box');
+		
+					   if (correctTextBox && incorrectTextBox) {
+						 correctTextBox.innerHTML = this.correctWords.join('<br>');
+						 incorrectTextBox.innerHTML = this.incorrectWords.join('<br>');
+					   }
+					 } else {
+					   console.log('User not found');
+					 }
+				  }, (error) => {
+					console.error('Error fetching user data:', error);
+				  });
+			  }
+			}
+		  }
   	
 	// 	// const formData = new FormData();
 	// 	// formData.append('audio', blob, 'recorded_audio.webm');
